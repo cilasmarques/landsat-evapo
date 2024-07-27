4e1f913c7ed5361f21c3c5cd0538df0c198593c2
cilasmarques <cilas@copin.ufcg.edu.br>
2024-07-27 03:19:13 -0300

    fix: adjusts the calculation of some products

--- src/cpp/products.cpp
+++ src/cpp/products.cpp
@@ -194,13 +194,13 @@ void Products::reflectance_function(MTL mtl, Sensor sensor)
   {
     for (int i = 0; i < this->height_band * this->width_band; i++)
     {
-      this->reflectance1[i] = (PI * this->radiance1[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[1][sensor.ESUN] * sin_sun);
-      this->reflectance2[i] = (PI * this->radiance2[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[2][sensor.ESUN] * sin_sun);
-      this->reflectance3[i] = (PI * this->radiance3[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[3][sensor.ESUN] * sin_sun);
-      this->reflectance4[i] = (PI * this->radiance4[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[4][sensor.ESUN] * sin_sun);
-      this->reflectance5[i] = (PI * this->radiance5[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[5][sensor.ESUN] * sin_sun);
-      this->reflectance6[i] = (PI * this->radiance6[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[6][sensor.ESUN] * sin_sun);
-      this->reflectance7[i] = (PI * this->radiance7[i] * mtl.distance_earth_sun * mtl.distance_earth_sun) / (sensor.parameters[7][sensor.ESUN] * sin_sun);
+      this->reflectance1[i] = (PI * this->radiance1[i]) / (sensor.parameters[1][sensor.ESUN] * sin_sun);
+      this->reflectance2[i] = (PI * this->radiance2[i]) / (sensor.parameters[2][sensor.ESUN] * sin_sun);
+      this->reflectance3[i] = (PI * this->radiance3[i]) / (sensor.parameters[3][sensor.ESUN] * sin_sun);
+      this->reflectance4[i] = (PI * this->radiance4[i]) / (sensor.parameters[4][sensor.ESUN] * sin_sun);
+      this->reflectance5[i] = (PI * this->radiance5[i]) / (sensor.parameters[5][sensor.ESUN] * sin_sun);
+      this->reflectance6[i] = (PI * this->radiance6[i]) / (sensor.parameters[6][sensor.ESUN] * sin_sun);
+      this->reflectance7[i] = (PI * this->radiance7[i]) / (sensor.parameters[7][sensor.ESUN] * sin_sun);
     }
   }
 
@@ -212,23 +212,30 @@ void Products::reflectance_function(MTL mtl, Sensor sensor)
 
 void Products::albedo_function(MTL mtl, Sensor sensor)
 {
-  float *last_band = this->reflectance7;
-  float last_param = sensor.parameters[7][sensor.WB];
-
-  if (mtl.number_sensor == 8) {
-    last_band = this->reflectance6;
-    last_param = sensor.parameters[6][sensor.WB];
-  }
-
-  // According to the recommendations of Trezza et al. (2013)
-  for (int i = 0; i < this->height_band * this->width_band; i++)
+  if (mtl.number_sensor == 8)
   {
-    this->albedo[i] = this->reflectance1[i] * sensor.parameters[1][sensor.WB] +
-                      this->reflectance2[i] * sensor.parameters[2][sensor.WB] +
-                      this->reflectance3[i] * sensor.parameters[3][sensor.WB] +
-                      this->reflectance4[i] * sensor.parameters[4][sensor.WB] +
-                      this->reflectance5[i] * sensor.parameters[5][sensor.WB] +
-                      last_band[i] * last_param;
+    for (int i = 0; i < this->height_band * this->width_band; i++)
+    {
+      this->albedo[i] = this->reflectance1[i] * sensor.parameters[1][sensor.WB] +
+                        this->reflectance2[i] * sensor.parameters[2][sensor.WB] +
+                        this->reflectance3[i] * sensor.parameters[3][sensor.WB] +
+                        this->reflectance4[i] * sensor.parameters[4][sensor.WB] +
+                        this->reflectance5[i] * sensor.parameters[5][sensor.WB] +
+                        this->reflectance6[i] * sensor.parameters[6][sensor.WB];
+    }
+  }
+  else
+  {
+    for (int i = 0; i < this->height_band * this->width_band; i++)
+    {
+      float alb = this->reflectance1[i] * sensor.parameters[1][sensor.WB] +
+                  this->reflectance2[i] * sensor.parameters[2][sensor.WB] +
+                  this->reflectance3[i] * sensor.parameters[3][sensor.WB] +
+                  this->reflectance4[i] * sensor.parameters[4][sensor.WB] +
+                  this->reflectance5[i] * sensor.parameters[5][sensor.WB] +
+                  this->reflectance7[i] * sensor.parameters[7][sensor.WB];
+      this->albedo[i] = (alb - 0.03) / (this->tal[i] * this->tal[i]);
+    }
   }
 }
 
@@ -255,8 +262,8 @@ void Products::lai_function()
 {
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    float savi= ((1 + 0.05) * (this->reflectance4[i] - this->reflectance3[i])) / (0.05 + (this->reflectance4[i] + this->reflectance3[i]));
-    this->savi[i]  = savi;
+    float savi = ((1 + 0.5) * (this->reflectance4[i] - this->reflectance3[i])) / (0.5 + (this->reflectance4[i] + this->reflectance3[i]));
+    this->savi[i] = savi;
 
     if (!isnan(savi) && savi > 0.687)
       this->lai[i] = 6;
@@ -284,11 +291,11 @@ void Products::enb_emissivity_function()
 {
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    if (this->lai[i] == 0) 
+    if (this->lai[i] == 0)
       this->enb_emissivity[i] = NAN;
     else
       this->enb_emissivity[i] = 0.97 + 0.0033 * this->lai[i];
-  
+
     if ((ndvi[i] < 0) || (lai[i] > 2.99))
       this->enb_emissivity[i] = 0.98;
   }
@@ -350,7 +357,7 @@ void Products::surface_temperature_function(MTL mtl)
 
 void Products::short_wave_radiation_function(MTL mtl)
 {
-  double costheta = sin(mtl.sun_elevation * PI / 180);
+  float costheta = sin(mtl.sun_elevation * PI / 180);
 
   for (int i = 0; i < this->height_band * this->width_band; i++)
     this->short_wave_radiation[i] = (1367 * costheta * this->tal[i]) / (mtl.distance_earth_sun * mtl.distance_earth_sun);
@@ -360,16 +367,16 @@ void Products::large_wave_radiation_surface_function()
 {
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    double temperature_pixel = this->surface_temperature[i];
-    double surface_temperature_pow_4 = temperature_pixel * temperature_pixel * temperature_pixel * temperature_pixel;
+    float temperature_pixel = this->surface_temperature[i];
+    float surface_temperature_pow_4 = temperature_pixel * temperature_pixel * temperature_pixel * temperature_pixel;
     this->large_wave_radiation_surface[i] = this->eo_emissivity[i] * 5.67 * 1e-8 * surface_temperature_pow_4;
   }
 };
 
 void Products::large_wave_radiation_atmosphere_function(double temperature)
 {
-  double temperature_kelvin = temperature + 273.15;
-  double temperature_kelvin_pow_4 = temperature_kelvin * temperature_kelvin * temperature_kelvin * temperature_kelvin;
+  float temperature_kelvin = temperature + 273.15;
+  float temperature_kelvin_pow_4 = temperature_kelvin * temperature_kelvin * temperature_kelvin * temperature_kelvin;
 
   for (int i = 0; i < this->height_band * this->width_band; i++)
     this->large_wave_radiation_atmosphere[i] = this->ea_emissivity[i] * 5.67 * 1e-8 * temperature_kelvin_pow_4;
@@ -395,7 +402,7 @@ void Products::soil_heat_flux_function()
   {
     if (definitelyLessThan(this->ndvi[i], 0) || definitelyGreaterThan(this->ndvi[i], 0))
     {
-      double ndvi_pixel_pow_4 = this->ndvi[i] * this->ndvi[i] * this->ndvi[i] * this->ndvi[i];
+      float ndvi_pixel_pow_4 = this->ndvi[i] * this->ndvi[i] * this->ndvi[i] * this->ndvi[i];
       this->soil_heat[i] = (this->surface_temperature[i] - 273.15) * (0.0038 + 0.0074 * this->albedo[i]) *
                            (1 - 0.98 * ndvi_pixel_pow_4) * this->net_radiation[i];
     }
@@ -409,15 +416,15 @@ void Products::soil_heat_flux_function()
 
 void Products::d0_fuction()
 {
-  double CD1 = 20.6;
-  double HGHT = 4;
+  float CD1 = 20.6;
+  float HGHT = 4;
 
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    double pai = this->pai[i];
-    double cd1_pai_root = sqrt(CD1 * pai);
+    float pai = this->pai[i];
+    float cd1_pai_root = sqrt(CD1 * pai);
 
-    double DISP = HGHT * ((1 - (1 / cd1_pai_root)) + (pow(exp(1.0), -cd1_pai_root) / cd1_pai_root));
+    float DISP = HGHT * ((1 - (1 / cd1_pai_root)) + (pow(exp(1.0), -cd1_pai_root) / cd1_pai_root));
     if (pai < 0)
     {
       DISP = 0;
@@ -429,40 +436,40 @@ void Products::d0_fuction()
 
 void Products::kb_function(double ndvi_max, double ndvi_min)
 {
-  double HGHT = 4;
+  float HGHT = 4;
 
-  double visc = 0.00001461;
-  double pr = 0.71;
-  double c1 = 0.320;
-  double c2 = 0.264;
-  double c3 = 15.1;
-  double cd = 0.2;
-  double ct = 0.01;
-  double sf_c = 0.3;
-  double sf_d = 2.5;
-  double sf_e = 4.0;
+  float visc = 0.00001461;
+  float pr = 0.71;
+  float c1 = 0.320;
+  float c2 = 0.264;
+  float c3 = 15.1;
+  float cd = 0.2;
+  float ct = 0.01;
+  float sf_c = 0.3;
+  float sf_d = 2.5;
+  float sf_e = 4.0;
 
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    double zom = this->zom[i];
-    double u_ast_ini_terra = this->ustar[i];
-    double PAI = this->pai[i];
+    float zom = this->zom[i];
+    float u_ast_ini_terra = this->ustar[i];
+    float PAI = this->pai[i];
 
-    double Re_star = (u_ast_ini_terra * 0.009) / visc;
-    double Ct_star = pow(pr, -0.667) * pow(Re_star, -0.5);
-    double beta = c1 - c2 * (exp((cd * -c3 * PAI)));
-    double nec_terra = (cd * PAI) / (beta * beta * 2);
+    float Re_star = (u_ast_ini_terra * 0.009) / visc;
+    float Ct_star = pow(pr, -0.667) * pow(Re_star, -0.5);
+    float beta = c1 - c2 * (exp((cd * -c3 * PAI)));
+    float nec_terra = (cd * PAI) / (beta * beta * 2);
 
-    double kb1_fst_part = (cd * VON_KARMAN) / (4 * ct * beta * (1 - exp(nec_terra * -0.5)));
-    double kb1_sec_part = (beta * VON_KARMAN * (zom / HGHT)) / Ct_star;
-    double kb1s = (pow(Re_star, 0.25) * 2.46) - 2;
+    float kb1_fst_part = (cd * VON_KARMAN) / (4 * ct * beta * (1 - exp(nec_terra * -0.5)));
+    float kb1_sec_part = (beta * VON_KARMAN * (zom / HGHT)) / Ct_star;
+    float kb1s = (pow(Re_star, 0.25) * 2.46) - 2;
 
-    double fc = 1 - pow((this->ndvi[i] - ndvi_max) / (ndvi_min - ndvi_max), 0.4631);
-    double fs = 1 - fc;
+    float fc = 1 - pow((this->ndvi[i] - ndvi_max) / (ndvi_min - ndvi_max), 0.4631);
+    float fs = 1 - fc;
 
-    double soil_moisture_day_rel = 0.33;
+    float soil_moisture_day_rel = 0.33;
 
-    double SF = sf_c + (1 / (1 + pow(exp(1.0), (sf_d - (sf_e * soil_moisture_day_rel)))));
+    float SF = sf_c + (1 / (1 + pow(exp(1.0), (sf_d - (sf_e * soil_moisture_day_rel)))));
 
     this->kb1[i] = ((kb1_fst_part * pow(fc, 2)) +
                     (kb1_sec_part * pow(fc, 2) * pow(fs, 2)) +
@@ -473,12 +480,12 @@ void Products::kb_function(double ndvi_max, double ndvi_min)
 
 void Products::zom_fuction(double A_ZOM, double B_ZOM)
 {
-  double HGHT = 4;
-  double CD = 0.01;
-  double CR = 0.35;
-  double PSICORR = 0.2;
+  float HGHT = 4;
+  float CD = 0.01;
+  float CR = 0.35;
+  float PSICORR = 0.2;
 
-  double gama;
+  float gama;
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
     gama = pow((CD + CR * (this->pai[i] / 2)), -0.5);
@@ -492,30 +499,30 @@ void Products::zom_fuction(double A_ZOM, double B_ZOM)
 
 void Products::ustar_fuction(double u10)
 {
-  double zu = 10;
+  float zu = 10;
 
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    double DISP = this->d0[i];
-    double zom = this->zom[i];
-    this->ustar[i] = (u10 * VON_KARMAN) / std::log((zu - DISP) / zom);
+    float DISP = this->d0[i];
+    float zom = this->zom[i];
+    this->ustar[i] = (u10 * VON_KARMAN) / log((zu - DISP) / zom);
   }
 };
 
 void Products::aerodynamic_resistance_fuction()
 {
-  double zu = 10.0;
+  float zu = 10.0;
 
   for (int i = 0; i < this->height_band * this->width_band; i++)
   {
-    double DISP = this->d0[i];
-    double zom = this->zom[i];
-    double zoh_terra = zom / pow(exp(1.0), (this->kb1[i]));
+    float DISP = this->d0[i];
+    float zom = this->zom[i];
+    float zoh_terra = zom / pow(exp(1.0), (this->kb1[i]));
 
-    double temp_kb_1_terra = log(zom / zoh_terra);
-    double temp_rah1_terra = (1 / (this->ustar[i] * VON_KARMAN));
-    double temp_rah2 = log(((zu - DISP) / zom));
-    double temp_rah3_terra = temp_rah1_terra * temp_kb_1_terra;
+    float temp_kb_1_terra = log(zom / zoh_terra);
+    float temp_rah1_terra = (1 / (this->ustar[i] * VON_KARMAN));
+    float temp_rah2 = log(((zu - DISP) / zom));
+    float temp_rah3_terra = temp_rah1_terra * temp_kb_1_terra;
 
     this->aerodynamic_resistance[i] = temp_rah1_terra * temp_rah2 + temp_rah3_terra;
   }
